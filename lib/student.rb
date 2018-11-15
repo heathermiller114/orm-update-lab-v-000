9405fa3846ddf348cb7fdf816d4039d7002ec8ee
Heather Miller <heather.miller114@gmail.com>
2018-11-15 02:28:13 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,7 +4,52 @@ class Student
 
   attr_accessor :id, :name, :grade
   
-  def initialize
+  def initialize (id = nil, name, grade)
+    @id = id
+    @name = name
+    @grade = grade
+  end
+  
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students(
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade INTEGER
+      )
+    SQL
+    
+    DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table
+    sql = DROP TABLE students
+    
+    DB[:conn].execute(sql)
+  end
+  
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+    SQL
+    
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
+  
+  def self.create(name:, grade:)
+    new_student = Student.new(name, grade)
+    new_student.save
+    new_student
+  end
+  
+  def self.new_from_db(row)
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    
 
 
 end
