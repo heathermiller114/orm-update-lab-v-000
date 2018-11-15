04814d813e24e6e0e489f8cf329d66c62d3d3eea
Heather Miller <heather.miller114@gmail.com>
2018-11-15 02:39:57 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -64,7 +64,9 @@ class Student
       self.new_from_db(row)
     end.first
   end
-    
-
-
+  
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? where name = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.name)
+  end
 end
